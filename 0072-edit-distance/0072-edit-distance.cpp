52c5cda3d8daa938291eea0c8881ee289789f5d8
Shivam Kumar Rao <raoshivam353@gmail.com>
2026-09-17 22:44:30 +0530

    Time: 73 ms (5.53%), Space: 84.5 MB (5.08%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int solveMem(string s1, string s2, int m, int n, vector<vector<int>>& dp)  
+    { 
+        if(m == 0) return n; 
+        if(n == 0) return m; 
+        if(dp[m][n] != -1)
+        return dp[m][n]; 
+        if(s1[m-1] == s2[n-1])  { 
+            return dp[m][n] = solveMem(s1, s2, m-1, n-1, dp); 
+       } 
+        int removeOp  = solveMem(s1, s2, m-1, n, dp); 
+        int insertOp  = solveMem(s1, s2, m, n-1, dp); 
+        int replaceOp = solveMem(s1, s2, m-1, n-1, dp); 
+        dp[m][n] = 1 + min({removeOp, insertOp, replaceOp}); 
+        return dp[m][n]; 
+    } 
+    int minDistance(string word1, string word2) {
+        int m = word1.length(); 
+        int n = word2.length(); 
+        vector<vector<int>> dp(m+1, vector<int>(n+1, -1)); 
+        return solveMem(word1, word2, m, n, dp); 
+    }
+};
