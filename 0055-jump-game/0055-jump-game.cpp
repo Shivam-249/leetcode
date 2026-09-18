baf640c967ccd64aa71a218b079b21bbf97b65b5
Shivam Kumar Rao <raoshivam353@gmail.com>
2026-09-18 19:20:43 +0530

    Time: 581 ms (8.2%), Space: 55.8 MB (6%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,24 +1,33 @@
 class Solution {
 public:
-    bool solveMem(vector<int>& nums, int index, vector<int>& dp)
+    bool solve(vector<int>& nums,int index)
     {
-        if(index >= nums.size() - 1)
-            return true;
-        if(dp[index] != -1)
-            return dp[index];
-        for(int jump = 1; jump <= nums[index]; jump++)
+        if(index>=nums.size()-1)
+        return true;
+        for(int jump=1;jump<=nums[index];jump++)
         {
-            if(solveMem(nums, index + jump, dp))
-            {
-                return dp[index] = 1;
-            }
+            if(solve(nums,index+jump))
+            return true;
         }
-        return dp[index] = 0;
+        return false;
     }
-    bool canJump(vector<int>& nums) 
+    bool solveMem(vector<int>&nums,int index,vector<int>&dp)
     {
-        int index = 0;
-        vector<int> dp(nums.size(), -1);
-        return solveMem(nums, index, dp);
+        if(index>=nums.size()-1)
+        return true;
+        if(dp[index]!=-1)
+        return dp[index];
+        for(int jump=1;jump<=nums[index];jump++)
+        {
+            if(solveMem(nums,index+jump,dp))
+            return dp[index]=1;
+        }
+        return dp[index]=0;
+    }
+    bool canJump(vector<int>& nums) {
+        //return solve(nums,0);
+        int index=0;
+        vector<int>dp(nums.size(),-1);
+        return solveMem(nums,index,dp);
     }
 };
