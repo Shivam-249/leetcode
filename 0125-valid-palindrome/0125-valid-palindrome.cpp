1a730b1b94619578d771bb78d749bfec04846704
Shivam Kumar Rao <raoshivam353@gmail.com>
2026-09-21 22:38:42 +0530

    Time: 0 ms (100%), Space: 9.9 MB (52.2%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int start=0,end=s.length()-1;
-        while(start<end)
+        int start = 0, end = s.length() - 1;
+
+        while(start < end)
         {
             if(!isalnum(s[start]))
-            start++;
+                start++;
 
             else if(!isalnum(s[end]))
-            end--;
+                end--;
 
             else
             {
-                if(tolower(s[start])!=tolower(s[end]))
-                return false;
-                
-                start++,end--;
+                if(tolower(s[start]) != tolower(s[end]))
+                    return false;
+
+                start++;
+                end--;
             }
         }
+
         return true;
     }
 };
